553b125b94c51401db5823c2d4dea823a9bca6f8
bplmp <lourb896@newschool.edu>
2017-02-19 23:38:36 -0300

    Add caret on menu

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -6,16 +6,22 @@ $(document).ready(function() {
     var viewportHeight = document.documentElement.clientHeight;
     var footer = $("#footer");
     var footerMenu = $("#footer-menu");
+    var caret = $("#journeys-caret");
     if (!active) {
       body.css({ overflow: "hidden" });
       footer.animate({height: viewportHeight - 60}, 500, function() {
         // Animation complete.
         footerMenu.show();
+        caret.removeClass("fa-caret-up");
+        caret.addClass("fa-caret-down");
       });
       active = true;
     } else {
       body.css({ overflow: "auto" });
-      footer.animate({height: 70}, 500);
+      footer.animate({height: 70}, 500, function () {
+        caret.removeClass("fa-caret-down");
+        caret.addClass("fa-caret-up");
+      });
       footerMenu.hide();
       active = false;
     }
