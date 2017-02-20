55da28230dc90b7e0dd2c90945ad67e77612f138
bplmp <lourb896@newschool.edu>
2017-02-19 23:33:11 -0300

    Add sliding up menu

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
-  $('#read-more').click(function() {
+  var active = false;
+  $('#journeys-button').click(function() {
     var link = $(this);
-    var moreText = $("#more-info");
-    var journeysButton = $("#journeys-button");
-    moreText.show();
-    link.hide();
-    journeysButton.css({
-      width: "100%"
+    var body = $("body");
+    var viewportHeight = document.documentElement.clientHeight;
+    var footer = $("#footer");
+    var footerMenu = $("#footer-menu");
+    if (!active) {
+      body.css({ overflow: "hidden" });
+      footer.animate({height: viewportHeight - 60}, 500, function() {
+        // Animation complete.
+        footerMenu.show();
+      });
+      active = true;
+    } else {
+      body.css({ overflow: "auto" });
+      footer.animate({height: 70}, 500);
+      footerMenu.hide();
+      active = false;
+    }
     });
-  });
 });
